50646933d2c37a0cb3dd6b141d7c06958b98825f
AlanNgaiii <634620668@qq.com>
2022-09-05 16:28:00 +0800

    补充注释

--- make-doc.js
+++ make-doc.js
@@ -2,6 +2,6 @@
   文档生成
 */
 const fs = require("fs");
-const jsdoc2md = require("jsdoc-to-markdown");
+const jsdoc2md = require("jsdoc-to-markdown");// https://github.com/jsdoc2md/jsdoc-to-markdown
 const output = jsdoc2md.renderSync({ files: "./index.js" });
 fs.writeFileSync("./README.md", output);
